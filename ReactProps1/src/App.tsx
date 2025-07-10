2b3607b7c4f049248a1024cf119feb3e023ede2d
buban10 <bubanezelmarie@gmail.com>
2025-07-10 16:31:08 +0800

    Initial commit

---
+++ ReactProps1/src/App.tsx
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import './App.css';
+import ProductList from './components/ProductList/ProductList';
+
+function App() {
+  const [capuccinoQty, setCapuccinoQty] = useState(0);
+  const [matchaQty, setMatchaQty] = useState(0);
+  const [chocolateQty, setChocolateQty] = useState(0);
+  const [rockyRoadQty, setRockyRoadQty] = useState(0);
+
+  const increaseQty = (product) => {
+    if (product === 'capuccino') setCapuccinoQty(capuccinoQty + 1);
+    if (product === 'matcha') setMatchaQty(matchaQty + 1);
+    if (product === 'chocolate') setChocolateQty(chocolateQty + 1);
+    if (product === 'rockyRoad') setRockyRoadQty(rockyRoadQty + 1);
+  };
+
+  const decreaseQty = (product) => {
+    if (product === 'capuccino' && capuccinoQty > 0) setCapuccinoQty(capuccinoQty - 1);
+    if (product === 'matcha' && matchaQty > 0) setMatchaQty(matchaQty - 1);
+    if (product === 'chocolate' && chocolateQty > 0) setChocolateQty(chocolateQty - 1);
+    if (product === 'rockyRoad' && rockyRoadQty > 0) setRockyRoadQty(rockyRoadQty - 1);
+  };
+
+  return (
+    <div className="container">
+      <ProductList
+        title="Capuccino"
+        imgURL="https://i.pinimg.com/736x/40/64/19/4064197377db3fc52f0b2c13af391e87.jpg"
+        description="This is Capuccino"
+        rating={3.8}
+        quantity={capuccinoQty}
+        onIncrease={() => increaseQty('capuccino')}
+        onDecrease={() => decreaseQty('capuccino')}
+      />
+
+      <ProductList
+        title="Matcha"
+        imgURL="https://i.pinimg.com/736x/7c/10/78/7c1078d4e23e24fdf8ae2555fe72eddc.jpg"
+        description="This is Matcha"
+        rating={4.8}
+        quantity={matchaQty}
+        onIncrease={() => increaseQty('matcha')}
+        onDecrease={() => decreaseQty('matcha')}
+      />
+
+      <ProductList
+        title="Chocolate"
+        imgURL="https://i.pinimg.com/736x/42/7d/7c/427d7c401321846d8751b48d0a801c22.jpg"
+        description="This is Chocolate"
+        rating={5.0}
+        quantity={chocolateQty}
+        onIncrease={() => increaseQty('chocolate')}
+        onDecrease={() => decreaseQty('chocolate')}
+      />
+
+      <ProductList
+        title="Rocky Road"
+        imgURL="https://i.pinimg.com/736x/d6/1a/f0/d61af04764020553f1865e3783aef090.jpg"
+        description="This is Rocky Road"
+        rating={10}
+        quantity={rockyRoadQty}
+        onIncrease={() => increaseQty('rockyRoad')}
+        onDecrease={() => decreaseQty('rockyRoad')}
+      />
+    </div>
+  );
+}
+
+export default App;
---
+++ ReactProps1/src/components/ProductList/ProductList.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function ProductList(props) {
+  return (
+    <div className="product-card">
+      <h4>{props.title}</h4>
+      <img src={props.imgURL} alt="Product Image" />
+      <p>{props.description}</p>
+      <p>Rating: {props.rating}</p>
+      {/* Removed the cart buttons and logic */}
+    </div>
+  );
+}
+
+export default ProductList;
